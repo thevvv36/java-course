0bd3ff0b3e66a785786ac709f68b081ee034f943
thevvv36 <30662995+thevvv36@users.noreply.github.com>
2020-05-04 00:37:13 +0200

    add some func

--- custom_order.java
+++ custom_order.java
@@ -12,10 +12,24 @@ public class custom_order {
     // TEST CODE
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
+	Scanner keyboard = new Scanner (System.in); 
+	String firstName; // User's first name
+	String itemOrder; // Item ordered
+	String frostingType; // Frosting ordered
+	String fillingType; // Filling ordered
+	String toppings; // Toppings ordered
+	String input;  // User input
+
+	double cost = 15.00; // Cost of cake and cupcakes
+	final double TAX_RATE = .08;  // Sales tax rate
+	double tax; // Amount of tax
 	  
 
 	 
-    // Introduce shop and prompt user to input first name
+	   // Introduce shop and prompt user to input first name
+
+	System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+	System.out.println("We make custom cakes with our secret cake batter!");
       
 
 	  
@@ -23,6 +37,15 @@ public class custom_order {
     // TEST CODE
     
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
+	System.out.println();
+	System.out.println(firstName + " , your order is as follows: ");
+	System.out.println("_________________________________________");
+	System.out.println("Item Ordered: " + itemOrder);
+	System.out.println("Frosting: " + frostingType);
+	System.out.println("Filling: " + fillingType);
+	System.out.println("Toppings: " + toppings);
+	System.out.println("_________________________________________");
+	
    
 	  
       
